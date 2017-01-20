ca8a081b61a82481418b6ba63ffed6e782717f2c
matthew jenkins <drakaven@hotmail.com>
2017-01-20 17:20:04 +0000

    added readme comments and fixed sent headers log

---
+++ README.md
@@ -0,0 +1,33 @@
+#TinyURL site
+
+## Expected Usage
+
+ The app will allow users to shorten long URLs much like TinyURL.com, bit.ly, and the like.
+
+## Dependencies
+
+"bcrypt": "^1.0.2",
+    "body-parser": "^1.15.2",
+    "cookie-parser": "^1.4.3",
+    "cookie-session": "^2.0.0-alpha.2",
+    "ejs": "^2.5.5",
+    "express": "^4.14.0",
+    "method-override": "^2.3.7",
+    "morgan": "^1.7.0",
+    "request": "^2.79.0"
+
+
+To install dependencies run:
+
+ npm install
+
+## Start
+
+  npm start
+
+## Notes
+
+  * /urls:id contains a log of each visit. Visits only update on reload not going back in the browser.
+  * you have to make sure you click the actual link on the buttons
+  * It is not a good looking site :)
+
--- express_server.js
+++ express_server.js
@@ -8,7 +8,6 @@ const express = require("express");
 const app = express();
 const PORT = 8080 || process.env.PORT; // default port 8080
 const bodyParser = require("body-parser");
-//const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session')
 const bcrypt = require('bcrypt');
 const methodOverride = require('method-override');
@@ -40,6 +39,7 @@ const uniqueVisitors = {};
 const validateUser = function(req, res) {
   if (!req.session.username) {
     res.status(401).send("You must be logged in to view this page <a href=http://localhost:8080/login>Login</a>");
+    return;
   }
 }
 
@@ -68,10 +68,14 @@ app.use(cookieSession({
 app.post("/register", (req, res) => {
   let userID = generateRandomString();
   if (!req.body.email || !req.body.password) res.status(400).send("Email address and password required!");
+  return;
   // this doesnt if we have no users
+  //check email for duplicate
   for (let userItem in users) {
     if (encodeURIComponent(req.body.email) === users[userItem].email) res.status(400).send("Email address in use please choose another");
+    return;
   }
+  //create user
   users[userID] = {
     id: userID,
     email: encodeURIComponent(req.body.email),
@@ -84,25 +88,27 @@ app.post("/register", (req, res) => {
 app.get("/register", (req, res) => {
   if (req.session.username) {
     res.redirect(302, 'http://localhost:8080/urls/');
+    return;
   }
   res.render("urls_registration")
 });
 
 app.post("/logout", (req, res) => {
-  //this clears the cookie but does not delete it
+  //this clears the cookie
   req.session = null;
   res.redirect(302, 'http://localhost:8080/');
 });
 app.get("/login", (req, res) => {
   if (req.session.username) {
     res.redirect(302, 'http://localhost:8080/urls/');
+    return;
   }
   res.render("urls_login");
 });
 
 app.post("/login", (req, res) => {
-  let passed = false
   for (let userItem in users) {
+    //if user name exists checkpass word and set cookie
     if (encodeURIComponent(req.body.email) === users[userItem].email) {
       if (bcrypt.compareSync(req.body.password, users[userItem].password) === true) {
         req.session.username = req.body.email;
@@ -110,12 +116,14 @@ app.post("/login", (req, res) => {
         return;
       } else {
         res.status(401).send("Password Incorrect");
+        return;
       }
     }
   };
   res.status(401).send("Username not found");
 });
 
+//generate randome string and add to urls DB
 app.post("/urls/create", (req, res) => {
   validateUser(req, res);
   let randString;
@@ -131,8 +139,9 @@ app.post("/urls/create", (req, res) => {
 });
 
 
-
+//delete link if logged in had belong to you
 app.delete("/urls/:shortURL/", (req, res) => {
+  validateUser(req, res);
   let key = req.url.replace("/urls/", "").replace("/?_method=DELETE", "");
   let encodeName = encodeURIComponent(req.session.username)
   if (encodeName === urlDatabase[key].user) {
@@ -141,21 +150,28 @@ app.delete("/urls/:shortURL/", (req, res) => {
   res.redirect(302, 'http://localhost:8080/urls');
 });
 
+//redirect to longurl
 app.get("/u/:shortURL", (req, res) => {
   if (!urlDatabase[req.params.shortURL]) {
     res.status(404).send("Short URL not found");
+    return;
   }
+  //analytics
   let shortURL = req.params.shortURL;
+  // check if logged in or has temp key
   let id = req.session.username || req.session.tempkey
+  // if no temp ket generat and set one
   if (id === undefined) {
     id = generateRandomString();
     req.session.tempkey = id;
   }
+  //add entrie to url log of the visits
   if (urlDatabase.hasOwnProperty(shortURL)) {
     urlDatabase[shortURL].log.push({
       id: id,
       time: Date.now()
     });
+    //add / update entry to unqiue vistior per url
     if (uniqueVisitors.hasOwnProperty([req.params.shortURL]))
     { uniqueVisitors[req.params.shortURL][id] = id}
     else {
@@ -163,6 +179,7 @@ app.get("/u/:shortURL", (req, res) => {
       uniqueVisitors[req.params.shortURL][id] = id;
     };
     res.redirect(302, urlDatabase[shortURL].url);
+    return;
   } else {
     res.redirect(302, 'http://localhost:8080/urls');
   }
@@ -184,28 +201,34 @@ app.get("/urls.user", (req, res) => {
   res.json(users);
 });
 
-//?
+//update url if exists logged in and belongs to you
 app.put("/urls/:id", (req, res) => {
   if (!urlDatabase[req.params.id]) {
     res.status(404).send("Short URL not found");
+    return;
   }
   validateUser(req, res);
   if (encodeURIComponent(req.session.username) !== urlDatabase[req.params.id].user) {
     res.status(403).send("You do not have access to this resource");
+    return;
   }
   urlDatabase[req.params.id].url = req.body.update;
   res.redirect(302, 'http://localhost:8080/urls/' + req.params.id);
 });
 
+// got url page if exists logged in and belongs to you
 app.get("/urls/:id", (req, res) => {
   if (!urlDatabase[req.params.id]) {
     res.status(404).send("Short URL not found");
+    return;
   }
   validateUser(req, res);
 
   if (encodeURIComponent(req.session.username) !== urlDatabase[req.params.id].user) {
     res.status(403).send("You do not have access to this resource");
+    return;
   }
+  //check unique visitor for url entry and send to page it exists
   let uniqueCount = 0;
   if (uniqueVisitors.hasOwnProperty(req.params.id)) {uniqueCount = Object.keys(uniqueVisitors[req.params.id]).length}
   let templateVars = {
@@ -217,6 +240,7 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+//get users' urls
 app.get("/urls", (req, res) => {
   validateUser(req, res);
   let templateVars = {
